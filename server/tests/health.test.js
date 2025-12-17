4f3169b70d96db6ded93cf9c7ec48f9626a6384c
NoaCohenScali <noacs2004@gmail.com>
2025-12-17 13:42:11 +0200

    Rechanged to original

--- server/tests/health.test.js
+++ server/tests/health.test.js
@@ -4,8 +4,7 @@ const app = require("../app");
 describe("Health check", () => {
   it("should return 200", async () => {
     const res = await request(app).get("/health");
-    // expect(res.statusCode).toBe(200);
-    expect(res.statusCode).toBe(500); 
+    expect(res.statusCode).toBe(200);
     expect(res.body.status).toBe("ok");
   });
 });
